6c320d2dca25caefa2ba2453bd33c5709ec5fff6
TwixKitty <rybothecamel@gmail.com>
2018-07-15 06:44:26 -0400

    Finished

--- syntax.js
+++ syntax.js
@@ -17,15 +17,67 @@ function parseSnippet(results, r, replacement) {
     for (var i = 0; i < results.length; i++) {
         if (results[i].grouped == false && results[i].value.search(r) != -1) {
             var new_results = parseSingleResult(results[i].value, r, replacement);
-            results = results.slice(0, 2);
-            return results.concat(parseSnippet(new_results, r, replacement));
+            results.splice.apply(results, [i, 1].concat(parseSnippet(new_results, r, replacement)));
         }
     }
     return results;
 }
-var code_snippets = document.getElementsByClassName("code");
-//parseSnippet(code_snippets[0].innerHTML);
-//let r : RegExp = new RegExp("(class)");
-var r = new RegExp("(public)");
-var results = parseSnippet([{ value: code_snippets[0].innerHTML, grouped: false }], r, "<span>$1</span>");
-console.log(results);
+function parseAllRoutines(s, routines) {
+    var results = [{ value: s, grouped: false }];
+    for (var i = 0; i < routines.length; i++) {
+        results = parseSnippet(results, routines[i].reg, routines[i].replacement);
+    }
+    // join all of the results into a friendly routine
+    var str = "";
+    for (var i = 0; i < results.length; i++) {
+        str += results[i].value;
+    }
+    return str;
+}
+/*
+* Function to escape html brackets so it doesn't interfere with the pre tag
+*/
+var entityMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+    '/': '&#x2F;',
+    '`': '&#x60;',
+    '=': '&#x3D;'
+};
+function escapeHtml(string) {
+    return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+        return entityMap[s];
+    });
+}
+function parseAllSnippets() {
+    var code_snippets = document.getElementsByClassName("code");
+    for (var i = 0; i < code_snippets.length; i++) {
+        var escaped = escapeHtml(code_snippets[i].innerHTML);
+        var t = parseAllRoutines(escaped, parseRoutines);
+        var pre = document.createElement("pre");
+        pre.innerHTML = t;
+        code_snippets[i].parentNode.appendChild(pre);
+        code_snippets[i].parentNode.removeChild(code_snippets[i]);
+    }
+}
+var parseRoutines = new Array();
+parseRoutines.push({ reg: new RegExp("(&#x2F;&#x2F;&#x2F;.*)"), replacement: "<span class='typescript_reference'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(&#x2F;&#x2F;.*)"), replacement: "<span class='typescript_comment'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(&#x2F;\\*[^*]*\\*+(?:[^&#x2F;*][^*]*\\*+)*&#x2F;)"), replacement: "<span class='typescript_multi_comment'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(Vector3|number)"), replacement: "<span class='typescript_type'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(class)"), replacement: "<span class='typescript_class'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(return)"), replacement: "<span class='typescript_return'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(if)"), replacement: "<span class='typescript_condition'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(else)"), replacement: "<span class='typescript_condition'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(undefined)"), replacement: "<span class='typescript_undefined'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(interface)"), replacement: "<span class='typescript_interface'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(enum)"), replacement: "<span class='typescript_enum'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(\\b(?<![A-Za-z])\\d+([.]\\d+)?)"), replacement: "<span class='typescript_number'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(static)"), replacement: "<span class='typescript_static'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(let)"), replacement: "<span class='typescript_variable'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(public)"), replacement: "<span class='typescript_public'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(constructor)"), replacement: "<span class='typescript_constructor'>$1</span>" });
+parseAllSnippets();
--- syntax.ts
+++ syntax.ts
@@ -38,24 +38,83 @@ function parseSnippet(results : Parse_Result[], r : RegExp, replacement : string
 	for(let i : number = 0;i < results.length;i++) {
 		if(results[i].grouped == false && results[i].value.search(r) != -1) {
 			let new_results : Parse_Result[] = parseSingleResult(results[i].value, r, replacement);
-			results = results.slice(0, 2);
-			return results.concat(parseSnippet(new_results, r, replacement));
+			results.splice(i, 1, ...parseSnippet(new_results, r, replacement));
 		}
 	}
 	
 	return results;
 }
 
+function parseAllRoutines(s : string, routines : Parse_Routine[]) : string {
+	let results : Parse_Result[] = [{value: s, grouped: false}];
+	for(let i : number = 0;i < routines.length;i++) {
+		results = parseSnippet(results, routines[i].reg, routines[i].replacement);
+	}
+	
+	// join all of the results into a friendly routine
+	
+	let str : string = "";
+	
+	for(let i : number = 0;i < results.length;i++) {
+		str += results[i].value;
+	}
+	
+	return str;
+}
 
+/*
+* Function to escape html brackets so it doesn't interfere with the pre tag
+*/
+var entityMap = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#39;',
+  '/': '&#x2F;',
+  '`': '&#x60;',
+  '=': '&#x3D;'
+};
 
-//let code_snippets : HTMLCollectionOf<Element> = document.getElementsByClassName("code");
+function escapeHtml(string) : string {
+	return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+		return entityMap[s];
+	});
+}
 
-//parseSnippet(code_snippets[0].innerHTML);
+function parseAllSnippets() {
+	let code_snippets : HTMLCollectionOf<Element> = document.getElementsByClassName("code");
 
-//let r : RegExp = new RegExp("(class)");
-//let r : RegExp = new RegExp("(public)");
+	for(let i = 0;i < code_snippets.length;i++) {
+		let escaped : string = escapeHtml(code_snippets[i].innerHTML);
+		let t : string = parseAllRoutines(escaped, parseRoutines);
 
+		let pre = document.createElement("pre");
 
-//let results : Parse_Result[] = parseSnippet([{value: code_snippets[0].innerHTML, grouped: false}], r, "<span>$1</span>");
+		pre.innerHTML = t;
 
-//console.log(results);
+		code_snippets[i].parentNode.appendChild(pre);
+		code_snippets[i].parentNode.removeChild(code_snippets[i]);
+	}
+}
+
+let parseRoutines : Parse_Routine[] = new Array();
+
+parseRoutines.push({reg : new RegExp("(&#x2F;&#x2F;&#x2F;.*)"), replacement : "<span class='typescript_reference'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(&#x2F;&#x2F;.*)"), replacement : "<span class='typescript_comment'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(&#x2F;\\*[^*]*\\*+(?:[^&#x2F;*][^*]*\\*+)*&#x2F;)"), replacement : "<span class='typescript_multi_comment'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(Vector3|number)"), replacement : "<span class='typescript_type'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(class)"), replacement : "<span class='typescript_class'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(return)"), replacement : "<span class='typescript_return'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(if)"), replacement : "<span class='typescript_condition'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(else)"), replacement : "<span class='typescript_condition'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(undefined)"), replacement : "<span class='typescript_undefined'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(interface)"), replacement : "<span class='typescript_interface'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(enum)"), replacement : "<span class='typescript_enum'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(\\b(?<![A-Za-z])\\d+([.]\\d+)?)"), replacement : "<span class='typescript_number'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(static)"), replacement : "<span class='typescript_static'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(let)"), replacement : "<span class='typescript_variable'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(public)"), replacement : "<span class='typescript_public'>$1</span>"});
+parseRoutines.push({reg : new RegExp("(constructor)"), replacement : "<span class='typescript_constructor'>$1</span>"});
+
+parseAllSnippets();
